3f452d2569b89545b3e16fe7e0ae817b4e9238bc
jrmarqueshd <jrmarqueshd@gmail.com>
2019-10-05 12:42:23 -0300

    finished frontend app on omnistack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'; // global para lidar com construção de interfaces
+import ReactDOM from 'react-dom'; // criado para lidar com contrução de arvore // funciona como uma engine
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
